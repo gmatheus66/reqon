1932d082a493329165fc87dc57e595e0dd52aefc
gmatheus66 <gmatheus66@hotmail.com>
2019-09-28 21:46:08 -0300

    GH-24 Criação da Migration de curso

---
+++ projeto/database/migrations/2019_09_29_003608_create_curso_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCursoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('curso', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome',15);
+            $table->string('duracao',10);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('curso');
+    }
+}
