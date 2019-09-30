9bbe4e4e8b23407d1484f8e37d9428cf1dec38f9
gmatheus66 <gmatheus66@hotmail.com>
2019-09-29 23:09:03 -0300

    GH-24 Criação da Migration de aluno

---
+++ projeto/database/migrations/2019_09_30_014315_create_aluno_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAlunoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('aluno', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome',50);
+            $table->string('email',50);
+            $table->date('data_nasc');
+            $table->string('cpf',11);
+            $table->char('glg_token',255);
+            $table->char('fcb_token',255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('aluno');
+    }
+}
