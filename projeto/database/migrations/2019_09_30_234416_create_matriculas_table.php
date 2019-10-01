d7d8c875453428b3b255b1950416b40e6ff10a50
gmatheus66 <gmatheus66@hotmail.com>
2019-09-30 21:18:36 -0300

    GH-24 Criação da Migration Matriculas

---
+++ projeto/database/migrations/2019_09_30_234416_create_matriculas_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMatriculasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('matriculas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('matricula',9);
+            $table->integer('ano');
+            $table->string('semestre',10);
+            $table->enum('status',['Cursando','Trancado','Concluido']);
+            $table->unsignedBigInteger('curso_id');
+            $table->unsignedBigInteger('aluno_id');
+            $table->unsignedBigInteger('req_id');
+            
+            $table->foreign('req_id')->references('id')->on('requerimentos');
+            $table->foreign('curso_id')->references('id')->on('cursos');
+            $table->foreign('aluno_id')->references('id')->on('alunos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('matriculas');
+    }
+}
