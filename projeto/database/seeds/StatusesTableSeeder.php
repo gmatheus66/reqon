8cce7d5a204f4819f75a84f1bf12483b46232174
gmatheus66 <gmatheus66@hotmail.com>
2019-09-30 23:37:15 -0300

    GH-28 Configuracao do seed e faker para popular a tabela status

---
+++ projeto/database/seeds/StatusesTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Status;
+use Illuminate\Database\Seeder;
+
+class StatusesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\Status::class,50)->create();
+    }
+}
