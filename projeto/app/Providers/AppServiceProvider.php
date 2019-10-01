37f63cfb8687988c21211238af90ad343cd7913d
gmatheus66 <gmatheus66@hotmail.com>
2019-09-30 23:33:40 -0300

    GH-28 Configurações para começar a popular a tabela

--- projeto/app/Providers/AppServiceProvider.php
+++ projeto/app/Providers/AppServiceProvider.php
@@ -13,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton(FakerGenerator::class, function(){
+            return FakerGenerator::create('pt_BR');
+        });
     }
 
     /**
