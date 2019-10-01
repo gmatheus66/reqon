ed9c08ee25c78d01e1019c27a1bcf3eb1cf17094
gmatheus66 <gmatheus66@hotmail.com>
2019-09-30 23:34:30 -0300

    GH-28 Criação do Model da Tabela Status

---
+++ projeto/app/Status.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Status extends Model
+{
+    protected $table = 'statuses';
+
+    protected $fillable = ['situacao'];
+
+}
