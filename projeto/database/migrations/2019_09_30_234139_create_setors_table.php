e06157bd8554d3b3d432dbc58698824526c79488
gmatheus66 <gmatheus66@hotmail.com>
2019-09-30 21:15:18 -0300

    GH-24 Criação da Migration Setors

---
+++ projeto/database/migrations/2019_09_30_234139_create_setors_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSetorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('setors', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome',60);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('setors');
+    }
+}
