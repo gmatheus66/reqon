5bcd320d8800284ab173a7775679aef08ba59b05
gmatheus66 <gmatheus66@hotmail.com>
2019-09-30 21:11:18 -0300

    GH-24 Criação da Migration Tipos

---
+++ projeto/database/migrations/2019_09_30_233051_create_tipos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTiposTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tipos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('descricao',10);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tipos');
+    }
+}
