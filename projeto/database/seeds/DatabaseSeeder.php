1bd90fa7c3c7d7deff733cd1743df5e3c7b4671e
gmatheus66 <gmatheus66@hotmail.com>
2019-10-06 18:00:06 -0300

    GH-46 Adição daseed Cursos

--- projeto/database/seeds/DatabaseSeeder.php
+++ projeto/database/seeds/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             StatusesTableSeeder::class,
             TiposTableSeeder::class,
-            SubtiposTableSeeder::class
+            SubtiposTableSeeder::class,
+            CursosTableSeeder::class
         ]);
 
     }
