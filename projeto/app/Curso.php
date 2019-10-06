7b1fe104715b8d00c63bc835164a063422b7f4b1
gmatheus66 <gmatheus66@hotmail.com>
2019-10-06 17:56:28 -0300

    GH-46 Criação do model Curso

---
+++ projeto/app/Curso.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Curso extends Model
+{
+    protected $table = 'cursos';
+
+    protected $fillable = ['nome','duracao'];
+}
