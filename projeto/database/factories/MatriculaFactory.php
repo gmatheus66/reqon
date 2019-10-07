271e84ac2c12202eee0e1c007aaa7b94ccbcda3f
GuiLira <liraguilherme252@gmail.com>
2019-10-06 23:10:32 -0300

    GH-45 Criação de factory para Matricula.

---
+++ projeto/database/factories/MatriculaFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Matricula;
+use Faker\Generator as Faker;
+
+$factory->define(Matricula::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
