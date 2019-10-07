011395a47525bd60f615a23f62b13baa8e11b834
gmatheus66 <gmatheus66@hotmail.com>
2019-10-07 02:12:57 -0300

    GH-45 Adiçao de colunas e modificação da matricula para expressões regulares

--- projeto/database/factories/MatriculaFactory.php
+++ projeto/database/factories/MatriculaFactory.php
@@ -7,8 +7,13 @@ use Faker\Generator as Faker;
 
 $factory->define(Matricula::class, function (Faker $faker) {
     return [
-        'matricula' => $faker->numberBetween(001, 999),
+       
+        'matricula' => $faker->regexify('20[0-1][0-9][A-Z]{5}[0-9]{4}'),
         'ano' => $faker->year($max = 'now'),
-        // 'campus' => $faker->campus
+        'semestre' => $faker->randomElement(['1 Periodo', '2 Periodo', '3 Periodo', '4 Periodo']),
+        'status' => $faker->randomElement(['Cursando','Trancado','Concluido']),
+        'curso_id'=> $faker->numberBetween(1,3),
+        'aluno_id' => $faker->numberBetween(1,10)
+     
     ];
 });
