26f918d904777dd1728a1056b5f9b409330ab56f
GuiLira <liraguilherme252@gmail.com>
2019-10-08 09:38:08 -0300

    GH-45 Otimização da factory de Matricula
    Criação de variaveis, para gerar dados que seram mais parecidos com os que seram usados futuramente.

--- projeto/database/factories/MatriculaFactory.php
+++ projeto/database/factories/MatriculaFactory.php
@@ -6,10 +6,17 @@ use App\Matricula;
 use Faker\Generator as Faker;
 
 $factory->define(Matricula::class, function (Faker $faker) {
+       
+    $ano = $faker->regexify('20[0-1][0-9]');
+    $campus = $faker->randomElement(['INFPL','INFRC','INFIG']);
+    $numero_matri = $faker->regexify('[0-9]{4}');
+    $matricula = $ano . $campus . $numero_matri; 
+
+
     return [
        
-        'matricula' => $faker->regexify('20[0-1][0-9][A-Z]{5}[0-9]{4}'),
-        'ano' => $faker->year($max = 'now'),
+        'matricula' => $matricula,
+        'ano' => $ano,
         'semestre' => $faker->randomElement(['1 Periodo', '2 Periodo', '3 Periodo', '4 Periodo']),
         'status' => $faker->randomElement(['Cursando','Trancado','Concluido']),
         'curso_id'=> $faker->numberBetween(1,3),
