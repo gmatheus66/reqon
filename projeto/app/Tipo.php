40965a0baa9e96116508080f16e537bf4f401aa6
gmatheus66 <gmatheus66@hotmail.com>
2019-10-09 10:53:18 -0300

    GH-40 Adição dos relacionamentos entre tipos e subtipos

--- projeto/app/Tipo.php
+++ projeto/app/Tipo.php
@@ -9,4 +9,8 @@ class Tipo extends Model
     protected $table = 'tipos';
 
     protected $fillable = ['descricao'];
+
+    public function subtipos() {
+        return $this->hasMany('App\Subtipo');
+    }
 }
