5839e9e3b6ff8e071c1a98259d7b051aaaf64877
gmatheus66 <gmatheus66@hotmail.com>
2019-10-09 10:54:07 -0300

    GH-40 Adição das rotas para o requerimento

--- projeto/routes/web.php
+++ projeto/routes/web.php
@@ -18,3 +18,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::resource('requerimento', 'RequerimentoController');
+
+Route::apiResource('requerimento','RequerimentoController');
+
