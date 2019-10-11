98a13c14291b7aa7cadd99fd3158c8410190e9ea
gmatheus66 <gmatheus66@hotmail.com>
2019-10-10 21:39:30 -0300

    GH-56 Adição do Caminho para criar o requerimento

--- projeto/resources/views/requerimento/index.blade.php
+++ projeto/resources/views/requerimento/index.blade.php
@@ -5,12 +5,11 @@
     <div class="contanier">
 
     <a href="{{ route('requerimento.create')}}">Criar Requerimento</a>
-    <ul>
-        @foreach($requerimento as $req)
-            <li>{{$req}}</li>
-        @endforeach
-    
-    </ul>
-    
+
+    @if(session()->get('sucesso'))
+        <div class="alert alert-info" role="alert">
+            {{ session()->get('sucesso') }}  
+        </div>
+    @endif
     </div>
 @endsection
