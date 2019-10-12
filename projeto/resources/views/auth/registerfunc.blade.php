e0969bc2c24bd43c133bb8ec120e480de4029d3b
GuiLira <liraguilherme252@gmail.com>
2019-10-12 20:58:15 -0300

    GH-54 Criação de nova view
    Criação de view para cadastro de funcionario.

--- projeto/resources/views/auth/registerfunc.blade.php
+++ projeto/resources/views/auth/registerfunc.blade.php
@@ -8,7 +8,7 @@
                 <div class="card-header">{{ __('Register') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
+                    <form method="POST" action="{{ route('registerfunc') }}">
                         @csrf
 
                         <div class="form-group row">
