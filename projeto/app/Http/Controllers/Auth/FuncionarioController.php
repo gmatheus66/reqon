12d1483f393c683a5e31265d1067859a000e6fc3
GuiLira <liraguilherme252@gmail.com>
2019-10-12 21:00:13 -0300

    GH-54 Crição de metodo
    Inicio da criação de metodo para autenticar cadastro.

--- projeto/app/Http/Controllers/Auth/FuncionarioController.php
+++ projeto/app/Http/Controllers/Auth/FuncionarioController.php
@@ -7,5 +7,38 @@ use Illuminate\Http\Request;
 
 class Funcionario extends Controller
 {
-    //
+    use RegistersUsers;
+
+    /**
+     * Where to redirect users after registration.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+    }
 }
