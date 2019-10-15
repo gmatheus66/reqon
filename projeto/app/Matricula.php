1550b3bdc3e5951588c30cb5fa648251497d506a
RenissonSilva <renissonx@gmail.com>
2019-10-15 00:02:31 -0300

    GH-65 Inserindo tables na model de matrícula

--- projeto/app/Matricula.php
+++ projeto/app/Matricula.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Matricula extends Model
 {
-    //
+    protected $table = 'matriculas';
+
+    protected $fillable = [
+    	'matricula',
+    	'ano',
+    	'semestre',
+    	'status'
+    ];
+
+    public function cursos() {
+        return $this->hasMany('App\Curso');
+    }
+
+    public function alunos() {
+        return $this->hasMany('App\Aluno');
+    }
+
+    protected $guarded = [
+        'id',
+        'created_at',
+        'update_at'
+    ];
 }
