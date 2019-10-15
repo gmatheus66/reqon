8cc20decafcff15f7c3b534d2911c87d983dd048
RenissonSilva <renissonx@gmail.com>
2019-10-14 23:03:24 -0300

    GH-43 Definição das tabelas da model

--- projeto/app/Setor.php
+++ projeto/app/Setor.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Setor extends Model
 {
-    //
+    protected $table = 'setors';
+
+    protected $fillable = ['nome'];
+
+    protected $guarded = [
+        'id',
+        'created_at',
+        'update_at'
+    ];
 }
