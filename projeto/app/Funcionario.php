f4d5b3c93b372300bfc29f7d9471e9f77bd6de14
GuiLira <liraguilherme252@gmail.com>
2019-10-14 22:24:45 -0300

    GH-54 Alteração no nome da tabela.

--- projeto/app/Funcionario.php
+++ projeto/app/Funcionario.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Funcionario extends Model
 {
-    protected $table = 'funcionario';
+    protected $table = 'funcionarios';
 
     protected $fillabel = [
         'cpf',
