b911ca48f7dd238a1654ca570d18c89ed10cfa75
pphenriquesm <pedrohsmarinho@outlook.com>
2019-10-16 03:29:02 -0300

    GH-77 remoção do bg-white

--- projeto/resources/views/layouts/app.blade.php
+++ projeto/resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm navClass">
+        <nav class="navbar navbar-expand-md navbar-light shadow-sm navClass">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('','ReqOn') }}
