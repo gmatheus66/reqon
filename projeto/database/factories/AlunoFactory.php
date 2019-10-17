51c0d6e5e02689c1a89b04b2703be5e74974d118
gmatheus66 <gmatheus66@hotmail.com>
2019-10-16 22:16:54 -0300

    GH-78 modificação na geração da hash da senha que estava sendo um gargalo na geração da seed de aluno

--- projeto/database/factories/AlunoFactory.php
+++ projeto/database/factories/AlunoFactory.php
@@ -9,9 +9,16 @@ use Illuminate\Support\Facades\Hash;
 $factory->define(Aluno::class, function (Faker $faker) {
     return [
         'nome' => $faker->name,
+        //'nome' => 'nomezinho',
+        //'data_nasc' => '2019-01-01',
+        //'email' =>  'tantofaz@gmail',
+        //'cpf' => '123',
+        //'password' => 'hello',
         'data_nasc' => $faker->date(),
         'email' => $faker->email,
         'cpf' => $faker->unique()->numerify('###########'),
-        'password' => Hash::make($faker->lexify('ipi2019'))
+        //'password' => Hash::make($faker->lexify('ipi2019'))
+        //'password' => Hash::make('ipi2019'),
+        //'password' => crypt('ipi2019')
     ];
 });
--- projeto/database/seeds/AlunosTableSeeder.php
+++ projeto/database/seeds/AlunosTableSeeder.php
@@ -2,6 +2,10 @@
 
 use App\Aluno;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\Hash;
 
 class AlunosTableSeeder extends Seeder
 {
@@ -12,6 +16,15 @@ class AlunosTableSeeder extends Seeder
      */
     public function run()
     {
-        factory('App\Aluno',10)->create();
+        $faker = Faker::create();
+        $pw = Hash::make($faker->lexify('ipi2019'));
+
+        DB::transaction(function() use ($pw) {
+            factory('App\Aluno',1000)->create([
+                'password' => $pw
+            ])->each(function($aluno) {
+                $aluno->matricula()->save(factory('App\Matricula', rand(1, 3)));
+            });
+        });
     }
 }
