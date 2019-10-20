6fcd52eb5a5df2d02ebb1ffe1bf1b4d245e1bbfa
GuiLira <liraguilherme252@gmail.com>
2019-10-19 23:25:35 -0300

    GH-75 Adição do nome da Model.

--- projeto/database/factories/FuncionarioFactory.php
+++ projeto/database/factories/FuncionarioFactory.php
@@ -2,21 +2,21 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Model;
+use App\Funcionario;
 use Faker\Generator as Faker;
 use Illuminate\Support\Facades\Hash;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(Funcionario::class, function (Faker $faker) {
     return [
-        'nome'=> $faker->$name,
+        'nome'=> $faker->name,
         'cpf'=> $faker->regexify('[0-9]{11}'),
         'rg_numero'=>$faker->regexify('[0-9]{8}'),
-        'rg_estado'=> $faker->randomElement('PE, PB, AM, BA'),
-        'rg_orgao_exp'=> $faker->randomElement('SDS, COREN, CORECON, DTP, DST'),
-        'cargo'=> $faker->randomElement('Professor, Servidor'),
-        'email'=> $faker->$email,
-        'password'=> Hash::make('ipi2019'),
-        'telefone'=> $faker->regexify('[0-9]{9}'),
-        'matricula'=>$faker->regexify('[0-9]{10}')
+        'rg_estado'=> $faker->randomElement(["PE", "PB", "AM", "BA"]),
+        'rg_orgao_exp'=> $faker->randomElement(["SDS", "COREN", "CORECON", "DTP", "DST"]),
+        'cargo'=> $faker->randomElement(["Professor", "Servidor"]),
+        'email'=> $faker->email,
+        'password'=> Hash::make("ipi2019"),
+        'telefone'=> $faker->regexify("[0-9]{9}"),
+        'matricula'=>$faker->regexify("[0-9]{10}")
     ];
 });
