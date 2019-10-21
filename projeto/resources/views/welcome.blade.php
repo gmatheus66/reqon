0dc35565890498c13f565cb65ce141f71751fc00
RenissonSilva <renissonx@gmail.com>
2019-10-21 19:02:35 -0300

    GH-79 Mudando link da home para redirecionar para tela de requerimento

--- projeto/resources/views/welcome.blade.php
+++ projeto/resources/views/welcome.blade.php
@@ -68,7 +68,7 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Início</a>
+                        <a href="{{ url('/requerimento') }}">Requerimentos</a>
                     @else
                         <a href="{{ route('login') }}">Entrar</a>
 <!-- 
