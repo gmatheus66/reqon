dc02360067862e18f8f477647f2b95f24ebf5487
GuiLira <liraguilherme252@gmail.com>
2019-10-22 00:42:44 -0300

    GH-70 Adição de rota para indexfunc.

--- projeto/routes/web.php
+++ projeto/routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::get('/funcindex', 'FuncionarioController@index');
+
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('registerfunc', 'FuncionarioController');
