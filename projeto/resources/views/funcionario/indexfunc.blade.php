6ea3ce5287596debe15fcfbfe52f6248f8050231
pphenriquesm <pedrohsmarinho@outlook.com>
2019-10-22 04:08:38 -0300

    GH-70 esboço da tela de visualização de requerimento pelo funcionário

--- projeto/resources/views/funcionario/indexfunc.blade.php
+++ projeto/resources/views/funcionario/indexfunc.blade.php
@@ -1,10 +1,19 @@
 @extends('layouts.app')
 
-
 @section('content')
-
-    @foreach ($reqs as $req)
-        <p>{{ $req }}</p>
+<div class="container">
+<div class="card  bg-light mb-3">
+  <h5 class="card-header">Requerimentos:</h5>
+  <div class="card-body">
+     @foreach ($reqs as $req)
+    <div>
+        <p>{{ $req["protocolo"] }}</p>
+        <p>{{ $req["descricao"] }}</p>
+        <p>{{ $req["subtipo_id"] }}</p>
+    </div>
     @endforeach
-
+     <a href="#" class="btn btn-primary">Detalhes</a>
+         </div>
+     </div>
+</div>
 @endsection
