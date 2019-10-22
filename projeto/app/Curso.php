dc42ccb7fbc1083b3d2076b6cafdf2ea5f76356e
gmatheus66 <gmatheus66@hotmail.com>
2019-10-21 23:45:10 -0300

    GH-80 Adição do Relacionamento de Curso

--- projeto/app/Curso.php
+++ projeto/app/Curso.php
@@ -9,4 +9,14 @@ class Curso extends Model
     protected $table = 'cursos';
 
     protected $fillable = ['nome','duracao'];
+
+    protected $guarded = [
+        'id',
+        'created_at',
+        'update_at'
+    ];
+
+    public function matricula(){
+        return $this->hasMany('App\Matricula', 'curso_id', 'id');
+    }
 }
