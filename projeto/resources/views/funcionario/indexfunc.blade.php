61efb20d7197012cd5c5986ca5cbb4c853253585
GuiLira <liraguilherme252@gmail.com>
2019-10-28 22:49:11 -0300

    GH-54 Pequenas midificações
    Adição de um if para verificar se usuario está logado.

--- projeto/resources/views/funcionario/indexfunc.blade.php
+++ projeto/resources/views/funcionario/indexfunc.blade.php
@@ -1,17 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
+@if(session('success')=='foi')
 <div class="container">
 <div class="card  bg-light mb-3">
   <h5 class="card-header">Requerimentos:</h5>
   <div class="card-body">
-     @foreach ($reqs as $req)
-    <div>
-        <p>{{ $req["protocolo"] }}</p>
-        <p>{{ $req["descricao"] }}</p>
-        <p>{{ $req["subtipo_id"] }}</p>
-    </div>
-    @endforeach
+      @foreach ($reqs as $req)
+            <div>
+                <p>{{ $req["protocolo"] }}</p>
+                <p>{{ $req["descricao"] }}</p>
+                <p>{{ $req["subtipo_id"] }}</p>
+            </div>
+        @endforeach
+    @else
+        @php
+            return redirect()->back()->withErrors('Você não está logado');
+        @endphp
+    @endif
      <a href="#" class="btn btn-primary">Detalhes</a>
          </div>
      </div>
