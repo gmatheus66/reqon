680a60ebe4a2ef8216ffd1ae11bec77e43ed6e8b
gmatheus66 <gmatheus66@hotmail.com>
2019-10-29 08:42:16 -0300

    GH-43 Retirando Segundo form do login do funcionario

--- projeto/resources/views/auth/login.blade.php
+++ projeto/resources/views/auth/login.blade.php
@@ -7,16 +7,16 @@
             <div class="card">
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
+                    <ul class="btn-group btn-group-toggle" data-toggle="buttons">
+                          <li class=" btn btn-secondary active" id="btn1" >
+                            <input type="radio" id="option1" autocomplete="off" checked name="aluno" value="aluno"> {{ __('Entrar como Aluno') }}
+                          </li>
+                          <li class=" btn btn-secondary" id="btn2" >
+                            <input type="radio" id="option2" autocomplete="off" name="funcionario" value="funcionario"> {{ __('Entrar como Funcionário') }}
+                          </li>
+                    </ul>
+                    <form id="form" method="POST" action="{{ route('login') }}">
                             @csrf
-                            <ul class="btn-group btn-group-toggle" data-toggle="buttons">
-                                  <li class=" btn btn-secondary active">
-                                    <input type="radio" id="option1" autocomplete="off" checked name="aluno" value="aluno"> {{ __('Entrar como Aluno') }}
-                                  </li>
-                                  <li class=" btn btn-secondary">
-                                    <input type="radio" id="option2" autocomplete="off" name="funcionario" value="funcionario"> {{ __('Entrar como Funcionário') }}
-                                  </li>
-                            </ul>
 
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
@@ -72,77 +72,7 @@
                             </div>
                         </div>
                     </form>
-                </div>
-            </div>
 
-
-            <div class="card">
-                <div class="card-body">
-                    <form method="POST" action="{{ route('admin') }}">
-                            @csrf
-                            <ul class="btn-group btn-group-toggle" data-toggle="buttons">
-                                  <li class=" btn btn-secondary active">
-                                    <input type="radio" id="option1" autocomplete="off" checked name="aluno" value="aluno"> {{ __('Entrar como Aluno') }}
-                                  </li>
-                                  <li class=" btn btn-secondary">
-                                    <input type="radio" id="option2" autocomplete="off" name="funcionario" value="funcionario"> {{ __('Entrar como Funcionário') }}
-                                  </li>
-                            </ul>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Senha') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Lembrar Senha') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Entrar') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Esqueceu a Senha?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
                 </div>
             </div>
 
@@ -150,4 +80,5 @@
         </div>
     </div>
 </div>
+
 @endsection
