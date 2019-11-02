831822b1cc5de1b0d1cc36f649bdacd7ea531ed1
RenissonSilva <renissonx@gmail.com>
2019-11-02 18:31:56 -0300

    GH-95 Criando um funcionario de cada setor na seeder

--- projeto/database/seeds/FuncionarioTableSeeder.php
+++ projeto/database/seeds/FuncionarioTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Funcionario;
+use Faker\Factory as Faker;
 
 class FuncionarioTableSeeder extends Seeder
 {
@@ -11,6 +13,43 @@ class FuncionarioTableSeeder extends Seeder
      */
     public function run()
     {
-        factory('App\Funcionario', 10)->create();
+    	$faker = Faker::create();
+    	Funcionario::create([
+	        'nome'=> 'CRADT',
+	        'cpf'=> $faker->regexify('[0-9]{11}'),
+	        'rg_numero'=>$faker->regexify('[0-9]{8}'),
+	        'rg_estado'=> $faker->randomElement(["PE", "PB", "AM", "BA"]),
+	        'rg_orgao_exp'=> $faker->randomElement(["SDS", "COREN", "CORECON", "DTP", "DST"]),
+	        'cargo'=> $faker->randomElement(["Professor", "Servidor"]),
+	        'email'=> 'cradt@login',
+	        'password'=> Hash::make("ipi2019"),
+	        'telefone'=> $faker->regexify("[0-9]{9}"),
+	        'matricula'=>$faker->regexify("[0-9]{10}")
+        ]);
+        Funcionario::create([
+	        'nome'=> 'DAEE',
+	        'cpf'=> $faker->regexify('[0-9]{11}'),
+	        'rg_numero'=>$faker->regexify('[0-9]{8}'),
+	        'rg_estado'=> $faker->randomElement(["PE", "PB", "AM", "BA"]),
+	        'rg_orgao_exp'=> $faker->randomElement(["SDS", "COREN", "CORECON", "DTP", "DST"]),
+	        'cargo'=> $faker->randomElement(["Professor", "Servidor"]),
+	        'email'=> 'daee@login',
+	        'password'=> Hash::make("ipi2019"),
+	        'telefone'=> $faker->regexify("[0-9]{9}"),
+	        'matricula'=>$faker->regexify("[0-9]{10}")
+        ]);
+        Funcionario::create([
+	        'nome'=> 'Coordenação',
+	        'cpf'=> $faker->regexify('[0-9]{11}'),
+	        'rg_numero'=>$faker->regexify('[0-9]{8}'),
+	        'rg_estado'=> $faker->randomElement(["PE", "PB", "AM", "BA"]),
+	        'rg_orgao_exp'=> $faker->randomElement(["SDS", "COREN", "CORECON", "DTP", "DST"]),
+	        'cargo'=> $faker->randomElement(["Professor", "Servidor"]),
+	        'email'=> 'coordena@login',
+	        'password'=> Hash::make("ipi2019"),
+	        'telefone'=> $faker->regexify("[0-9]{9}"),
+	        'matricula'=>$faker->regexify("[0-9]{10}")
+        ]);
+        factory('App\Funcionario', 7)->create();
     }
 }
