97cd53a2dcd11748a0ed19bd8c6c58e2b2ea1f47
RenissonSilva <renissonx@gmail.com>
2019-11-02 17:56:56 -0300

    GH-94 Austando seeder para que o segundo aluno fique com 2 matriculas

--- projeto/database/seeds/MatriculasTableSeeder.php
+++ projeto/database/seeds/MatriculasTableSeeder.php
@@ -2,6 +2,7 @@
 
 use App\Matricula;
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
 
 class MatriculasTableSeeder extends Seeder
 {
@@ -13,5 +14,14 @@ class MatriculasTableSeeder extends Seeder
     public function run()
     {
         factory('App\Matricula',10)->create();
+    	$faker = Faker::create();
+        Matricula::create([
+	        'matricula' => '2008INFRC2436',
+	        'ano' => '2008',
+	        'semestre' => $faker->randomElement(['1 Periodo', '2 Periodo', '3 Periodo', '4 Periodo']),
+	        'status' => $faker->randomElement(['Cursando','Trancado','Concluido']),
+	        'curso_id'=> $faker->numberBetween(1,3),
+	        'aluno_id' => 2
+        ]);
     }
 }
