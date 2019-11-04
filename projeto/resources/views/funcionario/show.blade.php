d9f70474ca15aad3d0d6319f078662917d139397
gmatheus66 <gmatheus66@hotmail.com>
2019-11-04 09:03:48 -0300

    GH-91 Criação da view para mostrar o requerimento selecionado

---
+++ projeto/resources/views/funcionario/show.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+
+@section('content')
+
+	    <div class="container" id="cont">
+			<div class="container-fluid">
+                @if($requerimento)
+                    @foreach ($requerimento as $req)
+
+                    <div class="row">
+                        <div class="col-lg-12 mb-4">
+                            <!-- Card -->
+                            <div class="card ex8-card">
+                            <!-- Card content -->
+
+                            <div class="card-body">
+                                <!-- Title -->
+                                <h4 class="card-title"><a>{{$req->subtipo->descricao}}</a></h4>
+                                <h6 class="card-title">Protocolo: {{$req->protocolo}}</h6>
+                                <h6 class="card-title">Data: {{date('d-m-Y', strtotime($req->created_at))}}</h6>
+                                <!-- Text -->
+                                <p class="card-text">Descricao: {{$req->descricao}}</p>
+                                <p class="card-text">Status: {{$req->status->situacao}}</p>
+                            </div>
+                            </div>
+                            <!-- Card -->
+                        </div>
+                    </div>
+                        @endforeach
+                        @endif
+            </div>
+
+		</div>
+
+@endsection
