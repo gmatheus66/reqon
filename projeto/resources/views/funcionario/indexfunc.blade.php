7138f3894eabb2fed639650d0e9e93aee7a9d049
gmatheus66 <gmatheus66@hotmail.com>
2019-11-04 08:56:37 -0300

    GH-96 Correção no loop que lista os requerimentos

--- projeto/resources/views/funcionario/indexfunc.blade.php
+++ projeto/resources/views/funcionario/indexfunc.blade.php
@@ -4,20 +4,19 @@
 @if(session('success')=='foi')
 <div class="container">
     @foreach ($reqs as $req)
-    @foreach ($req as $subt)
-        <div class="card  bg-light mb-3">
-            <h5 class="card-header">{{ $req['subtipo']->descricao }}</h5>
-                <div class="card-body">
-                    <div class="style">
-                        <p>Protocolo :{{ $req["protocolo"] }}</p>
-                        <p>Matricula: {{ $req["matricula"]->matricula }}</p>
-                        <p>Status: {{ $req["status"]->situacao }}</p>
-                        <p>Data: {{date('d-m-Y', strtotime($req->created_at))}}</p>
-                        <a href="#" class="btn btn-primary">Detalhes</a>
-                    </div>
+    <div class="card  bg-light mb-3">
+        <h5 class="card-header">{{ $req['subtipo']->descricao }}</h5>
+            <div class="card-body">
+                <div class="style">
+                    <p>Protocolo: {{ $req["protocolo"] }}</p>
+                    <p>Matricula: {{ $req["matricula"]->matricula }}</p>
+                    <p>Status: {{ $req["status"]->situacao }}</p>
+                    <p>Data: {{date('d-m-Y', strtotime($req->created_at))}}</p>
+                <a href="{{ route('showreqfunc',$req->id)}}" class="btn btn-primary">Detalhes</a>
                 </div>
             </div>
-        @endforeach
+    </div>
+
         @endforeach
     @else
         @php
