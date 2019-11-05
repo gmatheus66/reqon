48728a386749520f458d6271b88b4ffa791fbe56
gmatheus66 <gmatheus66@hotmail.com>
2019-11-05 08:26:33 -0300

    GH-98 Input para passar o id do curso selecionado

--- projeto/resources/views/requerimento/create.blade.php
+++ projeto/resources/views/requerimento/create.blade.php
@@ -13,7 +13,7 @@
                      <div class="alert alert-danger" role="alert">
                       {{ $message }}
                     </div>
-                     @endforeach 
+                     @endforeach
                 @endif
             <ul class="nav nav-tabs abas" id="myTab" role="tablist">
                 @foreach($tipo as $tp)
@@ -24,30 +24,30 @@
                     @else
                     <li class="nav-item">
                         <a class="nav-link abas" id="{{$tp->descricao}}-tab" data-toggle="tab" href="#{{$tp->descricao}}" role="tab" aria-controls="{{$tp->descricao}}" aria-selected="false">{{$tp->descricao}} </a>
-                    </li>   
+                    </li>
                     @endif
                 @endforeach
 
             </ul>
-                
+
             <div class="tab-content" id="myTabContent">
                 @foreach($tipo as $tp)
                     @if($tp->id == 1)
                     <div class="tab-pane fade active show" id="{{$tp->descricao}}" role="tabpanel" aria-label="{{$tp->descricao}}-tab">
                 @else
                     <div class="tab-pane fade" id="{{$tp->descricao}}" role="tabpanel" aria-label="{{$tp->descricao}}-tab">
-                @endif 
+                @endif
                     <div class="grupoSubT">
                     @foreach($tp->subtipos as $sub)
                         <div class="custom-control custom-radio sub mar">
                             <input type="radio" class="custom-control-input" id="sub-{{$sub->id}}" name="subtipo" value="{{$sub->id}}">
                             <label class="custom-control-label" for="sub-{{$sub->id}}">{{$sub->descricao}}</label>
-                    </div>   
+                    </div>
                     @endforeach
                     </div>
                     </div>
                 @endforeach
-                
+
             </div>
             <p class="infoP">Insira uma descrição</p>
             @if($errors->any())
@@ -56,19 +56,21 @@
                       {{ $message }}
                       <?php $cor='red'; ?>
                     </div>
-                     @endforeach 
+                     @endforeach
                 @endif
             <div class="form-group" id="desc">
                 <label for="Descricao">Descrição:</label>
                 <textarea class="form-control rounded-0 " id="descricao" value="{{ old('descricao') }}" rows="6" name="descricao" maxlength="1000" style="border-color: {{$cor ?? 'black'}};">{{old('descricao')}}</textarea>
             </div>
 
-            <button type="submit" class="btn btn-primary btn-lg btn-req">Criar requerimento</button>    
-        
+            <input type="hidden" name="curso" value="{{$curso}}">
+
+            <button type="submit" class="btn btn-primary btn-lg btn-req">Criar requerimento</button>
+
         </form>
-  
-    
-    
-    
+
+
+
+
     </div>
 @endsection
