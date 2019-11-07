b7f9018e419f3289a18c894add795070990b6066
gmatheus66 <gmatheus66@hotmail.com>
2019-11-06 23:41:28 -0300

    GH-111 Removendo Link Do JQuery

--- projeto/resources/views/layouts/app.blade.php
+++ projeto/resources/views/layouts/app.blade.php
@@ -10,7 +10,6 @@
     <title>{{ config('', 'ReqOn') }}</title>
 
     <!-- Scripts -->
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="{{ asset('js/script.js')}}"></script>
 
