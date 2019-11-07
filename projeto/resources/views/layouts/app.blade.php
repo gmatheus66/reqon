bb7ec60aa4b70085775e498455cd457fbc5bd771
RenissonSilva <renissonx@gmail.com>
2019-11-07 09:05:41 -0300

    GH-103 Retirando a breadcrumb do layout de app

--- projeto/resources/views/layouts/app.blade.php
+++ projeto/resources/views/layouts/app.blade.php
@@ -76,11 +76,6 @@
                 </div>
             </div>
         </nav>
-        <nav aria-label ="breadcrumb">
-            <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="/">Home</a></li>
-            </ol>
-        </nav>
 
 
         <main class="py-4">
