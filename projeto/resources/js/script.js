ba85c30279c4e309122a12a6988c115a342b2088
gmatheus66 <gmatheus66@hotmail.com>
2019-11-06 23:40:39 -0300

    GH-111 Mudando os scripts para funcionar com o jquery instalado por npm

--- projeto/resources/js/script.js
+++ projeto/resources/js/script.js
@@ -1,7 +1,11 @@
-console.log('runing');
+const $ =  require("jquery");
+
 $(document).ready(function(){
+    console.log('run');
+
     let btn1 = $("#btn1");
     let btn2 = $('#btn2');
+    let aba = $(".abas");
 
     btn1.on("click", function(evt){
         evt.preventDefault();
@@ -13,21 +17,18 @@ $(document).ready(function(){
         evt.preventDefault();
         $('#form').attr('action','/login/admin');
     });
-})
 
-$('document').ready(function(){
-    setTimeout(function(){
-        let msg = $("#sucessoReq");
-        $( "#sucessoReq" ).fadeOut(500);
-    },2000);
-})
-
-
-$(document).ready(function(){
-    let aba = $(".abas");
 
     aba.on("click", function(evt){
         evt.preventDefault();
             $('input:radio').prop("checked", false);
     });
+
+})
+
+$(document).ready(function(){
+    setTimeout(function(){
+        let msg = $("#sucessoReq");
+        $( "#sucessoReq" ).fadeOut(500);
+    },2000);
 })
