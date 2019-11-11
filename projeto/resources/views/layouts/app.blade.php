50c3de83a17ee0129bf088a7faa0c7c08dce03d3
RenissonSilva <renissonx@gmail.com>
2019-11-11 19:54:52 -0300

    GH-89 Estilizando navbar

--- projeto/resources/views/layouts/app.blade.php
+++ projeto/resources/views/layouts/app.blade.php
@@ -16,6 +16,7 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{ URL::asset('/css/app.css') }}">
