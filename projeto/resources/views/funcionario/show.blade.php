432c966e40292252170799076126acec3d4c461e
gmatheus66 <gmatheus66@hotmail.com>
2019-11-12 00:15:32 -0300

    GH-91 Historico da movimenttação do requerimento no funcionario

--- projeto/resources/views/funcionario/show.blade.php
+++ projeto/resources/views/funcionario/show.blade.php
@@ -9,41 +9,44 @@
             <span class="breadcrumb-item active itemBread" aria-current="page">Detalhes do Requerimento</span>
 
     </div>
-	    <div class="container" id="cont">
-             <div class="container-fluid">
-                @if($requerimento)
-                    @foreach ($requerimento as $req)
+    </div>
+    <div class="showreq">
 
-                    <div class="row">
-                        <div class="col-sm">
+        <div id="cont">
+            <div class="container-fluid">
+                @if($requerimento)
+                @foreach ($requerimento as $req)
+
+                <div class="row">
+                    <div class="col-sm">
                           Protocolo
                         </div>
                         <div class="col-sm">
-                          Matrícula
+                            Matrícula
                         </div>
                         <div class="col-sm">
-                          Nome do Aluno
+                            Nome do Aluno
                         </div>
                         <div class="col-sm">
-                          Curso
+                            Curso
                         </div>
                         <div class="col-sm">
-                          Tipo
+                            Tipo
                         </div>
                         <div class="col-sm">
                           Data
                         </div>
                         <div class="col-sm">
-                          Situação
+                            Situação
                         </div>
                         <div class="col-sm">
-                          Semestre
+                            Semestre
                         </div>
                         <div class="col-sm">
                           Status da matrícula
                         </div>
                         <div class="col-sm">
-                          Setor
+                            Setor
                         </div>
                         <div class="col-sm">
                           Descrição
@@ -54,16 +57,16 @@
                           {{$req['protocolo']}}
                         </div>
                         <div class="col-sm">
-                          {{$req['matricula']['matricula']}}
+                            {{$req['matricula']['matricula']}}
                         </div>
                         <div class="col-sm">
-                          {{$req->matricula->aluno->nome}}
+                            {{$req->matricula->aluno->nome}}
                         </div>
                         <div class="col-sm">
-                          {{$req->matricula->curso->nome}}
+                            {{$req->matricula->curso->nome}}
                         </div>
                         <div class="col-sm">
-                          {{$req['subtipo']['descricao']}}
+                            {{$req['subtipo']['descricao']}}
                         </div>
                         <div class="col-sm">
                           {{date('d-m-Y', strtotime($req['created_at']))}}
@@ -87,5 +90,52 @@
                     @endforeach
                 @endif
             </div>
+        </div>
+        <hr class="my-4">
+	    <div class="" id="cont">
+             <div class="container-fluid">
+                 @if($reqpai)
+                    @foreach ($reqpai as $req)
+
+                    <div class="row">
+
+                        <div class="col-sm">
+                          {{$req['protocolo']}}
+                        </div>
+                        <div class="col-sm">
+                          {{$req['matricula']['matricula']}}
+                        </div>
+                        <div class="col-sm">
+                            {{$req->matricula->aluno->nome}}
+                        </div>
+                        <div class="col-sm">
+                          {{$req->matricula->curso->nome}}
+                        </div>
+                        <div class="col-sm">
+                          {{$req['subtipo']['descricao']}}
+                        </div>
+                        <div class="col-sm">
+                            {{date('d-m-Y', strtotime($req['created_at']))}}
+                        </div>
+                        <div class="col-sm">
+                            {{$req['status']['situacao']}}
+                        </div>
+                        <div class="col-sm">
+                            {{$req['matricula']['semestre']}}
+                        </div>
+                        <div class="col-sm">
+                            {{$req['matricula']['status']}}
+                        </div>
+                        <div class="col-sm">
+                            {{$req['setor']['nome']}}
+                        </div>
+                        <div class="col-sm">
+                            {{$req['descricao']}}
+                        </div>
+                    </div>
+                    @endforeach
+                    @endif
+            </div>
+
         </div>
 @endsection
