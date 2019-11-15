bf6e1bc7b5cfd4db608eee4777345cd423bec2cb
gmatheus66 <gmatheus66@hotmail.com>
2019-11-14 23:20:00 -0300

    GH-124 Modificação dos links para login e para quando esta logado dos dois tipos de usuarios

--- projeto/resources/views/welcome.blade.php
+++ projeto/resources/views/welcome.blade.php
@@ -70,15 +70,15 @@
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/requerimento') }}">Requerimentos</a>
-                    @else
-                        <a href="{{ route('login') }}">Entrar</a>
-<!-- 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif -->
-                    @endauth
+
+                @if(Auth::guard('funcionario')->check())
+                    <a href="{{ url('/indexfunc') }}">Requerimentos</a>
+                @elseif(Auth::guard()->check())
+                    <a href="{{ url('/requerimento') }}">Requerimentos</a>
+                @else
+                    <a href="{{ route('login') }}">Entrar</a>
+                @endif
+
                 </div>
             @endif
             <div class="content">
