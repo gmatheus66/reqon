a6638d0409c7f1cd2943f4746156a0ad061c0ae0
gmatheus66 <gmatheus66@hotmail.com>
2019-11-15 18:29:52 -0300

    GH-76 Adicionando login a partir da matricula(SIAP) ou email para o funcionario

--- projeto/app/Http/Controllers/Auth/FuncController.php
+++ projeto/app/Http/Controllers/Auth/FuncController.php
@@ -43,4 +43,12 @@ class FuncController extends Controller
         return auth()->guard('funcionario');
     }
 
+    public  function username(){
+        $indetity = \request()->get('email');
+        $fieldName = filter_var($indetity, FILTER_VALIDATE_EMAIL) ? 'email' : 'matricula';
+        \request()->merge([$fieldName => $indetity]);
+        return $fieldName;
+    }
+
+
 }
