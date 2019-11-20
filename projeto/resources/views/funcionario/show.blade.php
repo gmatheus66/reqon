71fed42d3dd46bef04017b54a874a41910cbb0f2
GuiLira <liraguilherme252@gmail.com>
2019-11-20 12:21:08 -0300

    GH-97 Aleterações no from da view.

--- projeto/resources/views/funcionario/show.blade.php
+++ projeto/resources/views/funcionario/show.blade.php
@@ -94,72 +94,73 @@
         <hr class="my-4">
         <div class="" id="cont">
             <form action="{{ route('redirecionar') }}" method="post">
-            @csrf
-            <div class="container-fluid">
-                 @if($reqpai)
-                    @foreach ($reqpai as $req)
-                    <div class="col-sm btn-secondary status">
-                            {{$req['status']['situacao']}}
-                    </div>
-                    <div class="row">
-                        <div class="col-sm">
-                            Semestre
-                        </div>
-                        <div class="col-sm">
-                            Setor
-                        </div>
-                        <div class="col-sm">
-                            Descrição
-                        </div>
-                        <div class="col-sm">
-                            Para o Setor...
-                        </div>
-                        {{-- <div class="col-sm btn-secondary">
-                            {{$req['status']['situacao']}}
-                        </div> --}}
-                        <div class="w-100"></div>
+                @csrf
+                <div class="container-fluid">
 
-                        <input type="hidden" name="requerimento" value="{{$req['id']}}">
-                        <input type="hidden" name="matricula" value="{{$req['matricula']['id']}}">
-                        <input type="hidden" name="subtipo" value="{{$req['subtipo']['id']}}">
-                        <input type="hidden" name="status" value="{{$req['status']['id']}}">
-                        <input type="hidden" name="setor" value="{{$req['setor']['id']}}">
-                        <input type="hidden" name="descricao" value="{{$req['descricao']}}">
+                    @if($reqpai)
+                        @foreach ($reqpai as $req)
+                            <div class="col-sm btn-secondary status">
+                                    {{$req['status']['situacao']}}
+                            </div>
+                            <div class="row">
+                                <div class="col-sm">
+                                    Semestre
+                                </div>
+                                <div class="col-sm">
+                                    Setor
+                                </div>
+                                <div class="col-sm">
+                                    Descrição
+                                </div>
+                                <div class="col-sm">
+                                    Para o Setor...
+                                </div>
+                                {{-- <div class="col-sm btn-secondary">
+                                    {{$req['status']['situacao']}}
+                                </div> --}}
+                                <div class="w-100"></div>
 
-                        <div class="col-sm">
-                            {{$req['matricula']['semestre']}}
-                        </div>
-                        <div class="col-sm">
-                            {{$req['setor']['nome']}}
-                        </div>
-                        <div class="col-sm">
-                            {{$req['descricao']}}
-                        </div>
-                            <div class="form-group">
-                                <select name="teste" class="selectpicker form-control" >
-                                    <option value="1">CRADT</option>
-                                    <option value="2">COORDENAÇÃO</option>
-                                    <option value="3">DAEECINFO</option>
-                                    <option value="4">CINFO</option>
-                                </select>
+                                <input type="hidden" name="requerimento" value="{{$req['id']}}">
+                                <input type="hidden" name="matricula" value="{{$req['matricula']['id']}}">
+                                <input type="hidden" name="subtipo" value="{{$req['subtipo']['id']}}">
+                                <input type="hidden" name="status" value="{{$req['status']['id']}}">
+                                <input type="hidden" name="setor" value="{{$req['setor']['id']}}">
+                                <input type="hidden" name="descricao" value="{{$req['descricao']}}">
+
+                                <div class="col-sm">
+                                    {{$req['matricula']['semestre']}}
+                                </div>
+                                <div class="col-sm">
+                                    {{$req['setor']['nome']}}
+                                </div>
+                                <div class="col-sm">
+                                    {{$req['descricao']}}
+                                </div>
+                                    <div class="form-group">
+                                        <select name="teste" class="selectpicker form-control" >
+                                            <option value="1">CRADT</option>
+                                            <option value="2">COORDENAÇÃO</option>
+                                            <option value="3">DAEECINFO</option>
+                                            <option value="4">CINFO</option>
+                                        </select>
+                                    </div>
+
+
                             </div>
 
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                <button class="btn cmt" type="button" id="button-addon1">Comentário</button>
+                                </div>
+                                <textarea class="form-control comentario" name="comentario" aria-label="Comentario"></textarea>
+                            </div>
 
-                    </div>
-
-                    <div class="input-group mb-3">
-                        <div class="input-group-prepend">
-                          <button class="btn cmt" type="button" id="button-addon1">Comentário</button>
-                        </div>
-                        <textarea class="form-control comentario" name="comentario" aria-label="Comentario"></textarea>
-                    </div>
-
-                    <button type="submit" class="coment-btn">Enviar</button>
+                            <button type="submit" class="coment-btn">Enviar</button>
+                        @endforeach
+                    @else
+                        <p>teste</p>
+                    @endif
+                </div>
             </form>
-            </div>
-                    @endforeach
-                @elseif(!$reqpai)
-                <p>teste</p>
-                @endif
-            </div>
+        </div>
 @endsection
