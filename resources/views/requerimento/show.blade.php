8b858ffd17dfcc0038d696041c5c129316e46693
k0rgana <anagrom1999@gmail.com>
2019-11-26 20:49:43 -0300

    GH-118 alterando organização dos dados estudante

--- resources/views/requerimento/show.blade.php
+++ resources/views/requerimento/show.blade.php
@@ -25,34 +25,32 @@
 
                     <h3 class="subTitleReq">Dados do Estudante</h3>
 
-                    <div class="">                        
-                      Matrícula:{{$req['matricula']['matricula']}}
-
-                      Nome: {{$req->matricula->aluno->nome}}
-
-                      Status da matrícula:{{$req['matricula']['status']}}
-                  
-                      Curso:{{$req['matricula']['curso']['nome']}}
-
-                      Semestre:{{$req['matricula']['semestre']}}
-
+                    <div class=""> 
+                      <div class="row">
+                        <div class="col-sm">Matrícula: {{$req['matricula']['matricula']}}</div>
+                        <div class="col-sm">Status da matrícula: {{$req['matricula']['status']}}</div>
+                        <div class="col-sm">Curso: {{$req['matricula']['curso']['nome']}}</div>
+                      </div>                       
+                      <div class="row">
+                        <div class="col-sm-8">Nome: {{$req->matricula->aluno->nome}}</div>
+                        <div class="col-sm-4">Semestre: {{$req['matricula']['semestre']}}</div>
                       </div>
+                    </div>
 
                       <h3 class="subTitleReq">Dados do Requerimento</h3>
 
                       <div class="">
                         <div class="row">
                           <div class="col">Tipo: {{$req['subtipo']['descricao']}}</div>
-                          <div class="col">Situação:{{$req['status']['situacao']}}</div>
+                          <div class="col">Situação: {{$req['status']['situacao']}}</div>
                           <div class="col">Data: {{date('d-m-Y', strtotime($req['created_at']))}}</div>
                         </div>
                         <div class="row">
-                          <div class="col">Descrição:{{$req['descricao']}}</div>
-                          <div class="row align-items-center">
+                            <div class="col col-sm-8">Descrição:</div>
                             <div class="col">Setor:  {{$req['setor']['nome']}}</div>
                               <div class="w-100"></div> <!-- força a quebra de linha -->
-                            <div class="col">Protocolo : {{$req['protocolo']}}</div>
-                          </div>
+                            <div class="col col-sm-8">{{$req['descricao']}}</div>
+                            <div class="col">Protocolo: {{$req['protocolo']}}</div>
                         </div>
                         
                       </div>  
