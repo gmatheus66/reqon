a5697c38c80a68ac482a6ff0fe8cfefe22c9ef3b
gmatheus66 <gmatheus66@hotmail.com>
2019-11-27 08:28:21 -0300

    GH-97 Adicionando breadcumbs a nova tela

--- resources/views/funcionario/fulldet.blade.php
+++ resources/views/funcionario/fulldet.blade.php
@@ -1,4 +1,12 @@
 @extends('layouts.app')
 @section('content')
+    <div class="container">
+        <div class="container" id="breadcrumb">
+            <span class="itemBread"><a href="/">Início</a> ></span>
+            <span class="itemBread"><a href="/indexfunc">Requerimentos</a> ></span>
+            <span class="breadcrumb-item active itemBread" aria-current="page">Detalhes do Requerimento</span>
+
+        </div>
+    </div>
     @include('funcionario.det', [$requerimento, $status, $setor])
 @endsection
