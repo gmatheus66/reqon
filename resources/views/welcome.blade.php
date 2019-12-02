21af95652a2b78d078010f08fbb907bf34651335
RenissonSilva <renissonx@gmail.com>
2019-12-02 13:47:17 -0300

    GH-127 Inserindo navbar na welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>ReqOn</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -15,6 +15,8 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
         <link href="https://fonts.googleapis.com/css?family=Titillium+Web&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="{{ URL::asset('/css/app.css') }}">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <!-- Styles -->
         <style type="text/css">
             body{
@@ -81,11 +83,32 @@
     <body>
 
     <div class="header">
-        <nav class="my-nav navbar navbar-expand-lg navbar-light bg-light">
-          <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
-            <ul class="navbar-nav mr-auto">
-            </ul>
-            <form class="form-inline my-2 my-lg-0 linksWelcome">
+        <!-- <nav class="navbar navbar-light bg-light">            
+          <a class="navbar-brand-welcome">ReqOn</a>
+          <form class="form-inline linksWelcome">
+            <a href="/">INÍCIO</a>
+                    <a href="#">SOBRE</a>
+                    <a href="#">CONTATO</a>
+                    @if (Route::has('login'))
+                        @if(Auth::guard('funcionario')->check())
+                            <a href="{{ url('/indexfunc') }}">REQUERIMENTOS</a>
+                        @elseif(Auth::guard()->check())
+                            <a href="{{ url('/requerimento') }}">REQUERIMENTOS</a>
+                        @else
+                            <a href="{{ route('login') }}">ENTRAR</a>
+                        @endif
+                    @endif
+          </form>
+        </nav> -->
+        <nav class="navbar navbar-expand-md navbar-light shadow-sm navClassWelcome">
+            <div class="container"> 
+                <a class="navbar-brand-welcome" href="{{ url('/') }}"><img id="logoSite" src="{{ asset('img/logoSite-verde.png') }}">ReqOn</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="form-inline linksWelcome" id="navbarSupportedContent">
+
                     <a href="/">INÍCIO</a>
                     <a href="#">SOBRE</a>
                     <a href="#">CONTATO</a>
@@ -98,11 +121,9 @@
                             <a href="{{ route('login') }}">ENTRAR</a>
                         @endif
                     @endif
-            </form>
-          </div>
+                </div>
+            </div>
         </nav>
-            
-          <span class="ReqOn"><img id="logoVerde" src="{{ asset('img/logoSite-verde.png') }}">ReqOn</span>
 
             <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">      
                 <div class="carousel-inner">
