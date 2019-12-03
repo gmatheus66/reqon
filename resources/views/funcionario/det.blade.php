a47abd44b7664b212f063cfb65a711c12bbafed9
gmatheus66 <gmatheus66@hotmail.com>
2019-12-03 17:42:17 -0300

    GH-118 Modificando visualização dos requeurimentos filhos

--- resources/views/funcionario/det.blade.php
+++ resources/views/funcionario/det.blade.php
@@ -1,74 +1,73 @@
 
 <div class="showreq child">
-        <div class="icon collapse"></div>
 
-
-        <div class="dadosreq"><h3 class="subTitleReq">Dados do Requerimento</h3></div>
-
-        <div class="">
-           <div class="row">
-             <div class="col">Tipo: {{$requerimento['subtipo']['descricao']}}</div>
-             <div class="col">Situação: <h5 class="{{Str::slug($requerimento['status']['situacao'])}}" >{{$requerimento['status']['situacao']}}</h5></div>
-             <div class="col">Data: {{date('d-m-Y', strtotime($requerimento['created_at']))}}</div>
-           </div>
-           <div class="row">
-               <div class="col col-sm-8">Descrição:</div>
-               <div class="col">Setor:  {{$requerimento['setor']['nome']}}</div>
-                 <div class="w-100"></div> <!-- força a quebra de linha -->
-               <div class="col col-sm-8">{{$requerimento['descricao']}}</div>
-               <div class="col">Protocolo: {{$requerimento['protocolo']}}</div>
-           </div>
-           @if($requerimento->comentario)
-            <div class="row">
-                    <div class="col">Comentario: {{$requerimento->comentario}}</div>
-                </div>
-           @endif
+    <div class="child-header d-flex justify-content-between">
+    <div class="icon @if($open) collapse @else expand @endif" data-body="#body-{{$requerimento->id}}"></div>
+        <div class="p-1">
+            <span class="setor">{{$requerimento['setor']['nome']}}</span>
+            <span class="date">({{date('d-m-Y', strtotime($requerimento['created_at']))}})</span>
         </div>
-
-        <div id="btn-action">
-            <div class="row">
-                <button id="btn-encaminhar" class="coment-btn btn-lg col ">Encaminhar</button>
-                <button id="btn-responder" class="coment-btn btn-lg col ">Responder</button>
+        <div class="p-1">
+            <span class="protocol">#{{$requerimento['protocolo']}}</span>
+            <span class="status badge status-{{Str::slug($requerimento['status']['situacao'])}}">{{$requerimento['status']['situacao']}}</span>
+        </div>
+    </div>
+    <div class="child-body container-flex" id="body-{{$requerimento->id}}" @if(!$open) style="display:none" @endif>
+        <p>
+            {{$requerimento['descricao']}}
+        </p>
+        @if($requerimento->comentario)
+            <p class="answer">
+                {{$requerimento->comentario}}
+            </p>
+        @endif
+        <div class="row">
+            <div class="col-6">
+                <button class="btn-action btn btn-block btn-primary" data-form="#encaminhar-{{$requerimento->id}}">Encaminhar</button>
+            </div>
+            <div class="col-6">
+                <button class="btn-action btn btn-block btn-secondary" data-form="#responder-{{$requerimento->id}}">Responder</button>
             </div>
         </div>
-        <div id="action-responder" style="display: none">
-                <h3 class="subTitleReq">Responder</h3>
-                <div class="">
-                    <form action="{{ route('redirecionar') }}" method="post">
-                        @csrf
-                        <div class="row">
-                            <div class="form-group col">
-                                <select name="teste" class="selectpicker form-control" >
-                                    <option value="" selected>Selecione um Status</option>
-                                    @foreach ($status as $set)
-                                        <option value="{{$set['id']}}">{{$set['situacao']}}</option>
-                                    @endforeach
-                                </select>
-                            </div>
 
-                            <input type="hidden" name="requerimento" value="{{$requerimento['id']}}">
-                            <input type="hidden" name="matricula" value="{{$requerimento['matricula']['id']}}">
-                            <input type="hidden" name="subtipo" value="{{$requerimento['subtipo']['id']}}">
-                            <input type="hidden" name="status" value="{{$requerimento['status']['id']}}">
-                            <input type="hidden" name="setor" value="{{$requerimento['setor']['id']}}">
-                            <input type="hidden" name="descricao" value="{{$requerimento['descricao']}}">
-
+        <div class="form-action" id="responder-{{$requerimento->id}}" style="display: none">
+            <h3 class="subTitleReq">Responder</h3>
+            <div class="">
+                <form action="{{ route('redirecionar') }}" method="post">
+                    @csrf
+                    <div class="row">
+                        <div class="form-group col">
+                            <select name="teste" class="selectpicker form-control" >
+                                <option value="" selected>Selecione um Status</option>
+                                @foreach ($status as $set)
+                                    <option value="{{$set['id']}}">{{$set['situacao']}}</option>
+                                @endforeach
+                            </select>
                         </div>
-                        <div class="row">
-                            <div class="input-group col">
-                                <div class="input-group-prepend">
-                                    <button class="btn cmt" type="button" id="button-addon1">Comentário</button>
-                                    </div>
-                                    <textarea class="form-control comentario" name="comentario" aria-label="Comentario"></textarea>
+
+                        <input type="hidden" name="requerimento" value="{{$requerimento['id']}}">
+                        <input type="hidden" name="matricula" value="{{$requerimento['matricula']['id']}}">
+                        <input type="hidden" name="subtipo" value="{{$requerimento['subtipo']['id']}}">
+                        <input type="hidden" name="status" value="{{$requerimento['status']['id']}}">
+                        <input type="hidden" name="setor" value="{{$requerimento['setor']['id']}}">
+                        <input type="hidden" name="descricao" value="{{$requerimento['descricao']}}">
+
+                    </div>
+                    <div class="row">
+                        <div class="input-group col">
+                            <div class="input-group-prepend">
+                                <button class="btn cmt" type="button" id="button-addon1">Comentário</button>
                                 </div>
-                        </div>
-                        <div class="row">
-                            <button type="submit" class="coment-btn btn-lg col col-sm-2">Enviar</button>
-                        </div>
-                    </form>
-                </div>
+                                <textarea class="form-control comentario" name="comentario" aria-label="Comentario"></textarea>
+                            </div>
+                    </div>
+                    <div class="row">
+                        <button type="submit" class="coment-btn btn-lg col col-sm-2">Enviar</button>
+                    </div>
+                </form>
             </div>
-        <div id="action-encaminhar" style="display: none">
+        </div>
+        <div class="form-action" id="encaminhar-{{$requerimento->id}}" style="display: none">
             <h3 class="subTitleReq">Encaminhar</h3>
             <div class="">
                 <form action="{{ route('redirecionar') }}" method="post">
@@ -110,8 +109,11 @@
             </div>
         </div>
 
-                {{-- <p>{{ $requerimento->children }}</p> --}}
-                @foreach ($requerimento->children as $requerimento)
-                    @include('funcionario.det', [$requerimento, $status, $setor, $setorfunc])
-                @endforeach
+        @foreach ($requerimento->children as $requerimento)
+            @php
+                $open = false
+            @endphp
+            @include('funcionario.det', [$requerimento, $status, $setor, $setorfunc, $open])
+        @endforeach
+    </div>
 </div>
