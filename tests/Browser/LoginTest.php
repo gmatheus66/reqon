02332a6324163fff6a9445ca63cec12a08ad1766
RenissonSilva <renissonx@gmail.com>
2019-12-03 22:14:20 -0300

    GH-145 Criando testes para login de funcionário

--- tests/Browser/LoginTest.php
+++ tests/Browser/LoginTest.php
@@ -47,4 +47,49 @@ class LoginTest extends DuskTestCase
                     ->assertPathIs('/');
         });
     }
+    public function test_funcionario_deveria_nao_logar_com_a_senha_errada()
+    {
+        $this->browse(function(Browser $browser) {
+            $browser->visit('/')
+                    ->clickLink('ENTRAR')
+                    ->waitFor('form')
+                    ->click('[id=btn2]')
+                    ->waitFor('@option2')
+                    ->type('email', 'cradt@login')
+                    ->type('password', 'senha errada')
+                    ->click('[type=submit]')
+                    ->assertSee('As informações de login não foram encontradas');
+        });
+    }
+    public function test_funcionario_deveria_nao_logar_com_o_email_errado()
+    {
+        $this->browse(function(Browser $browser) {
+            $browser->visit('/')
+                    ->clickLink('ENTRAR')
+                    ->waitFor('form')
+                    ->click('[id=btn2]')
+                    ->waitFor('@option2')
+                    ->type('email', 'a@b')
+                    ->type('password', 'ipi2019')
+                    ->click('[type=submit]')
+                    ->assertSee('As informações de login não foram encontradas');
+        });
+    }
+    public function test_funcionario_deveria_logar()
+    {
+        $this->browse(function(Browser $browser) {
+            $browser->visit('/')
+                    ->clickLink('ENTRAR')
+                    ->waitFor('form')
+                    ->click('[id=btn2]')
+                    ->waitFor('@option2')
+                    ->type('email', 'cradt@login')
+                    ->type('password', 'ipi2019')
+                    ->click('[type=submit]')
+                    ->assertPathIs('/indexfunc')
+                    ->click('[id=navbarDropdown]')
+                    ->clickLink('Sair')
+                    ->assertPathIs('/');
+        });
+    }
 }
