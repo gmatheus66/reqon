58e29f2c3044359957b2bb58c842a50f9e95fc5a
pphenriquesm <pedrohsmarinho@outlook.com>
2019-12-04 00:36:28 -0300

    GH-143 atualizando dados do requerimento

--- app/Http/Controllers/RequerimentoController.php
+++ app/Http/Controllers/RequerimentoController.php
@@ -45,10 +45,11 @@ class RequerimentoController extends Controller
 
 }
         $update = Requerimento::where('id', $request->get('requerimento'))
-        ->update('setor_id', $request->get('setor'))
-        ->update('status_id',$request->get('teste'))
-        ->update('funcionario_id',Auth::user()->id)
-        ->update('comentario', $request->get('comentario'));
+        ->update(['setor_id' => $request->get('setor'),'status_id' => $request->get('teste'), 
+        'funcionario_id' => Auth::user()->id, 'comentario'=> $request->get('comentario')]);
+        //->update(['status_id' => $request->get('teste')])
+        //->update(['funcionario_id' => Auth::user()->id])
+        //->update(['comentario'=> $request->get('comentario')]);
 }
 
 
