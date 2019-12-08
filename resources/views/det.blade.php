3b721207edc83f37e80f4bb868d9e92f4ab106b4
GuiLira <liraguilherme252@gmail.com>
2019-12-08 17:08:02 -0300

    GH-153 Pequenas alterações
    Alteração para funcionario so comentar se requerimento estver no seu setor.

--- resources/views/det.blade.php
+++ resources/views/det.blade.php
@@ -25,101 +25,105 @@
     @if(Auth::guard('funcionario')->check())
         <div class="row">
             <div class="col-6">
-                <button class="btn-action btn btn-block btn-primary" data-form="#encaminhar-{{$requerimento->id}}">Encaminhar</button>
+                <button class="btn-action btn btn-block btn-primary btn-size" data-form="#encaminhar-{{$requerimento->id}}">Encaminhar</button>
             </div>
             <div class="col-6">
-                <button class="btn-action btn btn-block btn-secondary" data-form="#responder-{{$requerimento->id}}">Responder</button>
+                <button class="btn-action btn btn-block btn-secondary btn-size" data-form="#responder-{{$requerimento->id}}">Responder</button>
             </div>
         </div>
-
-        <div class="form-action" id="responder-{{$requerimento->id}}" style="display: none">
-            <h3 class="subTitleReq">Responder</h3>
-            <div class="">
-                <form action="{{ route('resposta') }}" method="post">
-                    @csrf
-                    <div class="row">
-                        <div class="form-group col">
-                            <select name="teste" class="selectpicker form-control" >
-                                <option value="" selected>Selecione um Status</option>
-                                @foreach ($status as $set)
-                                    <option value="{{$set['id']}}">{{$set['situacao']}}</option>
-                                @endforeach
-                            </select>
-                        </div>
-
-                        <input type="hidden" name="requerimento" value="{{$requerimento['id']}}">
-                        <input type="hidden" name="matricula" value="{{$requerimento['matricula']['id']}}">
-                        <input type="hidden" name="subtipo" value="{{$requerimento['subtipo']['id']}}">
-                        <input type="hidden" name="status" value="{{$requerimento['status']['id']}}">
-                        <input type="hidden" name="setor" value="{{$requerimento['setor']['id']}}">
-                        <input type="hidden" name="descricao" value="{{$requerimento['descricao']}}">
-
-                    </div>
-                    <div class="row">
-                        <div class="input-group col">
-                            <div class="input-group-prepend">
-                                <button class="btn cmt" type="button" id="button-addon1">Comentário</button>
-                                </div>
-                                <textarea class="form-control comentario" name="comentario" aria-label="Comentario"></textarea>
+        <p>{{$setor_nome}}</p>
+        @if ($setor_nome != Auth::user()->nome)
+            <div class="form-action" id="responder-{{$requerimento->id}}" style="display: none">
+                <h3 class="subTitleReq">Responder</h3>
+                <div class="">
+                    <form action="{{ route('resposta') }}" method="post">
+                        @csrf
+                        <div class="row">
+                            <div class="form-group col">
+                                <select name="teste" class="selectpicker form-control" >
+                                    <option value="" selected>Selecione um Status</option>
+                                    @foreach ($status as $set)
+                                        <option value="{{$set['id']}}">{{$set['situacao']}}</option>
+                                    @endforeach
+                                </select>
                             </div>
-                    </div>
-                    <div class="row">
-                        <button type="submit" class="coment-btn btn-lg col col-sm-2">Enviar</button>
-                    </div>
-                </form>
+
+                            <input type="hidden" name="requerimento" value="{{$requerimento['id']}}">
+                            <input type="hidden" name="matricula" value="{{$requerimento['matricula']['id']}}">
+                            <input type="hidden" name="subtipo" value="{{$requerimento['subtipo']['id']}}">
+                            <input type="hidden" name="status" value="{{$requerimento['status']['id']}}">
+                            <input type="hidden" name="setor" value="{{$requerimento['setor']['id']}}">
+                            <input type="hidden" name="descricao" value="{{$requerimento['descricao']}}">
+
+                        </div>
+                        <div class="row">
+                            <div class="input-group col">
+                                <div class="input-group-prepend">
+                                    <button class="btn cmt" type="button" id="button-addon1">Comentário</button>
+                                    </div>
+                                    <textarea class="form-control comentario" name="comentario" aria-label="Comentario">{{Input::old('comentario')}}</textarea>
+                                </div>
+                        </div>
+                        <div class="row">
+                            <button type="submit" class="coment-btn btn-lg col col-sm-2">Enviar</button>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </div>
-        <div class="form-action" id="encaminhar-{{$requerimento->id}}" style="display: none">
-            <h3 class="subTitleReq">Encaminhar</h3>
-            <div class="">
-                <form action="{{ route('redirecionar') }}" method="post">
-                    @csrf
-                    <div class="row">
-                        <div class="form-group col">
-                            <select name="teste" class="selectpicker form-control" data-live-search="true">
-                                <option value="" selected>Selecione um Setor</option>
-                                <optgroup label="Setores" data-max-options="2">
-                                @foreach ($setor as $set)
-                                    @if($setorfunc)
-                                    @foreach ($setorfunc as $setfunc)
-                                        @if ($set['id'] != $setfunc['setor_id'])
-                                            <option value="{{$set['id']}}">{{$set['nome']}}</option>
+            <div class="form-action" id="encaminhar-{{$requerimento->id}}" style="display: none">
+                @method('Symfony\Component\Console\Input\Input')
+                <h3 class="subTitleReq">Encaminhar</h3>
+                <div class="">
+                    <form action="{{ route('redirecionar') }}" method="post">
+                        @csrf
+                        <div class="row">
+                            <div class="form-group col">
+                                <select name="teste" class="selectpicker form-control" data-live-search="true">
+                                    <option value="" selected>Selecione um Setor</option>
+                                    <optgroup label="Setores" data-max-options="2">
+                                    @foreach ($setor as $set)
+                                        @if($setorfunc)
+                                        @foreach ($setorfunc as $setfunc)
+                                            @if ($set['id'] != $setfunc['setor_id'])
+                                                <option value="{{$set['id']}}">{{$set['nome']}}</option>
+                                            @endif
+                                        @endforeach
                                         @endif
                                     @endforeach
-                                    @endif
-                                @endforeach
-                                </optgroup>
-                                <optgroup label="Professores" data-max-options="2">
-                                @foreach ($professor as $prof)
-                                    <option value="{{$prof['id']}}">{{$prof['nome']}}</option>
-                                @endforeach
-                                </optgroup>
-                            </select>
-                        </div>
-
-                        <input type="hidden" name="requerimento" value="{{$requerimento['id']}}">
-                        <input type="hidden" name="matricula" value="{{$requerimento['matricula']['id']}}">
-                        <input type="hidden" name="subtipo" value="{{$requerimento['subtipo']['id']}}">
-                        <input type="hidden" name="status" value="{{$requerimento['status']['id']}}">
-                        <input type="hidden" name="setor" value="{{$requerimento['setor']['id']}}">
-                        <input type="hidden" name="descricao" value="{{$requerimento['descricao']}}">
-
-                    </div>
-                    <div class="row">
-                        <div class="input-group col">
-                            <div class="input-group-prepend">
-                                <button class="btn cmt" type="button" id="button-addon1">Comentário</button>
-                                </div>
-                                <textarea class="form-control comentario" name="comentario" aria-label="Comentario"></textarea>
+                                    </optgroup>
+                                    <optgroup label="Professores" data-max-options="2">
+                                    @foreach ($professor as $prof)
+                                        <option value="{{$prof['id']}}">{{$prof['nome']}}</option>
+                                    @endforeach
+                                    </optgroup>
+                                </select>
                             </div>
-                    </div>
-                    <div class="row">
-                        <button type="submit" class="coment-btn btn-lg col col-sm-2">Enviar</button>
-                    </div>
-                </form>
+
+                            <input type="hidden" name="requerimento" value="{{$requerimento['id']}}">
+                            <input type="hidden" name="matricula" value="{{$requerimento['matricula']['id']}}">
+                            <input type="hidden" name="subtipo" value="{{$requerimento['subtipo']['id']}}">
+                            <input type="hidden" name="status" value="{{$requerimento['status']['id']}}">
+                            <input type="hidden" name="setor" value="{{$requerimento['setor']['id']}}">
+                            <input type="hidden" name="descricao" value="{{$requerimento['descricao']}}">
+
+                        </div>
+                        <div class="row">
+                            <div class="input-group col">
+                                <div class="input-group-prepend">
+                                    <button class="btn cmt" type="button" id="button-addon1">Comentário</button>
+                                    </div>
+                                    <textarea class="form-control comentario" name="comentario" aria-label="Comentario"></textarea>
+                                </div>
+                        </div>
+                        <div class="row">
+                            <button type="submit" class="coment-btn btn-lg col col-sm-2">Enviar</button>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </div>
         @endif
+
+    @endif
         @foreach ($requerimento->children as $requerimento)
             @php
                 $open = false
