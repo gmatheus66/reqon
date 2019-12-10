f72e7d31691ef6558fd88709aaf40f5dcdd0193a
k0rgana <anagrom1999@gmail.com>
2019-12-10 00:25:14 -0300

    GH-149 adicionando logos verdes no footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -93,6 +93,52 @@
             </div>
         </div>
         </div>
-        @include('footer')
+
+<!-- Footer -->
+<footer class="page-footer font-small stylish-color-dark pt-4 footerClass">
+  <!-- Footer Links -->
+  <div class="text-center text-md-left">
+    <!-- Grid row -->
+    <div class="row a">
+      <!-- Grid column -->
+      <div class="col-md-1 mx-auto">
+        <!-- Content -->
+        <img class="footerL" src="{{ asset('img/footerImg-verde.png') }}">
+    </div>
+        <hr class="clearfix w-100 d-md-none">
+        <div class="col-md-4 mx-auto">
+            <p class="desc">Bem vindo ao ReqOn, o lugar onde você pode solicitar e acompanhar requerimentos online , tudo de forma fácil e gratuita.</p>
+      </div>
+      <!-- Grid column -->
+      <hr class="clearfix w-100 d-md-none">
+      <!-- Grid column -->
+      <div class="col-md-auto mx-auto">
+        <!-- Links -->
+        <h5 class="font-weight-bold text-uppercase mt-3 mb-4 titleFooter">Links Importantes</h5>
+        <ul class="list-unstyled linkFooter">
+          <li>
+            <a href="{{ url('/') }}">Início</a>
+          </li>
+          <li>
+            <a href="{{ url('/sobre') }}">Sobre</a>
+          </li>
+          <li>
+            <a href="{{ url('/contato') }}">Contato</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <!-- Grid row -->
+  </div>
+
+  <!-- Copyright -->
+  <div class="inline">
+    <img id="logoIF" src="{{ asset('img/logoIF-verde.png') }}">
+    <div class="footer-copyright text-center py-3">Todos os direitos reservados - IFPE Campus Igarassu
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
     </body>
 </html>
