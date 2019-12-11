324c90bb84fc52fafb92738ad863cc78678e72d6
pphenriquesm <pedrohsmarinho@outlook.com>
2019-12-11 15:24:41 -0300

    GH-148 colocando o m minusculo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -21,7 +21,7 @@
                             @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-3 col-form-label text-md-right">{{ __('E-Mail') }}</label>
+                            <label for="email" class="col-md-3 col-form-label text-md-right">{{ __('E-mail') }}</label>
                             <div class="col-md-9">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
                                 @error('email')
